80e16dcf9b1a63411b973d3c5abd4776094ebe70
aman <amanmaksat0@gmail.com>
2023-03-02 22:58:46 +0600

    Main

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -16,36 +16,41 @@ const Main = () => {
 
   return loading ? <Box sx={{textAlign: 'center'}}><CircularProgress/></Box> : (
     <Grid container direction="column" spacing={2}>
-      <Grid item container justifyContent="space-between" alignItems="center">
-        <Grid item>
-          <Typography variant="h3" fontFamily="Baltica">
-            Товары
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid item container spacing={3}>
-        {products.length !== 0 ? products.map(product => {
-          let disabled = false
+      {products?.length !== 0 ? (
+        <>
+          <Grid item container justifyContent="space-between" alignItems="center">
+            <Grid item>
+              <Typography variant="h3" fontFamily="Baltica">
+                Товары
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item container spacing={3}>
+            {products.map(product => {
+              let disabled = false
 
-          if (basket?.length) {
-            basket.forEach(item => {
-              if (item._id === product._id) disabled = true
-            })
-          }
+              if (basket?.length) {
+                basket.forEach(item => {
+                  if (item._id === product._id) disabled = true
+                })
+              }
 
-          return (
-            <ProductItem
-              key={product._id}
-              _id={product._id}
-              title={product.title}
-              price={product.price}
-              image={product.image}
-              stock={product.stock}
-              disabled={disabled}
-            />
-          )
-        }) : <Typography variant="h3" margin="30px 0 0 10%">No products</Typography>}
-      </Grid>
+              return (
+                <ProductItem
+                  key={product._id}
+                  _id={product._id}
+                  title={product.title}
+                  price={product.price}
+                  image={product.image}
+                  stock={product.stock}
+                  disabled={disabled}
+                />
+              )
+            })}
+          </Grid>
+        </>
+      ) : <Typography variant="h3" mangin="150px 20%" color="red">Временно нет товаров</Typography>
+      }
     </Grid>
   );
 };
